4681934f49f1446d784e4ffcf1ba1244a6d5ef75
awesomeprikolist <awesome.prikoolist@yandex.ru>
2023-02-21 14:51:01 +0300

    lab №2

---
+++ 2lab/4.py
@@ -0,0 +1,14 @@
+a = input()
+m = input()
+r = 'красный'
+b = 'синий'
+y = 'желтый'
+p = 'фиолетовый'
+o = 'оранжевый'
+g = 'зеленый'
+if a == r and m == b:
+    print(p)
+if a == r and m == y:
+    print(o)
+if a == b and m == y:
+    print(g)
