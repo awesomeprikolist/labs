9dadd7454675ccb5d1df64f9f12d0a35c2eac3ba
awesomeprikolist <awesome.prikole@yandex.ru>
2023-03-20 22:07:03 +0300

    5 lab

---
+++ 5lab/1.py
@@ -0,0 +1,8 @@
+spisok = [6, 13, 23, 36, 48]
+
+number = int(input("Введите число: "))
+
+if number in spisok:
+    print("Поздравляю, Вы угадали число!")
+else:
+    print("Нет такого числа!")
---
+++ 5lab/4.py
@@ -0,0 +1,23 @@
+import random
+
+group1 = ["Иванов", "Петров", "Сидоров", "Смирнов", "Козлов","Николаев","Егоров","Анисимов","Александров","Алексеев"]
+group2 = ["Кузнецов", "Попов", "Васильев", "Морозов", "Федоров","Фролов","Исаев","Максимов","Воронов","Зайцев"]
+
+# Создаем спортивную команду, выбирая случайные фамилии
+sports_team = random.sample(group1, 5) + random.sample(group2, 5)
+
+print("Группа 1:", group1)
+
+print("Группа 2:", group2)
+print("Спортивная команда:", sports_team)
+
+print("Длина списка:", len(sports_team))
+
+sorted_team = sorted(sports_team)
+
+surname = "Иванов"
+if surname in sorted_team:
+    print(f"Фамилия {surname} есть в команде")
+    print(f"Он встречается {sorted_team.count(surname)} раз(a)")
+else:
+    print(f"Фамилия {surname} не найдена в команде")
