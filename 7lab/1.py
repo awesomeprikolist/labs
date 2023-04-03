eb384f255f611967a1aa5de04a338fb669083607
awesomeprikolist <awesome.prikole@yandex.ru>
2023-04-03 21:00:53 +0300

    jest

---
+++ 7lab/1.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+image = Image.open('photo.jpg')
+
+height, width = image.size
+
+format = image.format
+
+mode = image.mode
+
+print(f'Размер изображения: ', image.size)
+print(f'Формат изображения: ', image.format)
+print(f'Цветовая модель изображения: ', image.mode)
+
+image.show()
