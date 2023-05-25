2113e17f42b59e9685a7b50fe8d835493f9856b9
awesome prikolist <125361929+awesomeprikolist@users.noreply.github.com>
2023-05-25 15:02:35 +0300

    Update 2.py

--- 10lab/2.py
+++ 10lab/2.py
@@ -1,16 +1,13 @@
 import json
 
-# загружаем данные из файла
 with open('products.json') as json_file:
     data = json.load(json_file)
 
-# запрашиваем новые данные у пользователя
 name = input('Введите название продукта: ')
 price = int(input('Введите цену продукта: '))
 weight = int(input('Введите вес продукта: '))
 available = input('В наличии? (Да/Нет): ').lower() == 'да'
 
-# создаем новый объект продукта и добавляем его к списку продуктов
 new_product = {
     'name': name,
     'price': price,
@@ -19,11 +16,9 @@ new_product = {
 }
 data['products'].append(new_product)
 
-# записываем обновленные данные в файл
 with open('products.json', 'w') as json_file:
     json.dump(data, json_file)
 
-# выводим обновленные данные на экран
 for product in data['products']:
     print('Название: ' + product['name'])
     print('Цена: ' + str(product['price']))
